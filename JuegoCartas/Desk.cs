dbe3304c88ef2b1f883487ba492d62c701d0c6c4
Nicks <1313nicov@gmail.com>
2022-04-02 23:48:30 -0500

    Set Up Deck + Enfrentamiento
    Se hizo el coco con los parametros y metodos inicales de La baraja y el enfrentamiento -Aka campo de Batlla-

--- JuegoCartas/Desk.cs
+++ JuegoCartas/Desk.cs
@@ -4,5 +4,22 @@ public class Desk
 {
 	public Desk()
 	{
+		String deskName = "";
+		int charcaterAmount = 0;
+		int equipAmount = 0;
+		int supportAmount = 0;
+
+		//Aquí va a llamar la agregación de las cartas
 	}
+
+	public void LlenarDeck()
+    {
+		//Llena la baraja con las cartas
+    }
+	public void DestruirDeck()
+    {
+		//Destruye el conido de la baraja
+
+    }
+
 }
--- JuegoCartas/Enfrentamiento.cs
+++ JuegoCartas/Enfrentamiento.cs
@@ -2,7 +2,14 @@
 
 public class Enfrentamiento
 {
-	public Enfrentamiento()
-	{
-	}
+    public Enfrentamiento()
+    {
+        String winner = "Aún no poseemos un ganador de este encuentro";
+
+    }
+    
+    public void BatallaAMuerte()
+    {
+        //Aquí se van a agarrar las dos barajas
+    }
 }
