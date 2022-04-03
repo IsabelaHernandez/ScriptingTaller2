98d301c08c7d609ccc740adf18cb8144df0c1f97
Isa <isabelahdez03@gmail.com>
2022-04-02 20:38:34 -0500

    Creacion_Proyecto

---
+++ Pruebas_Cartas/UnitTest1.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Pruebas_Cartas
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+        }
+    }
+}
