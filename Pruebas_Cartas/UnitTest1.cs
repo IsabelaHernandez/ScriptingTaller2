e15aad7a61d1d17f42c161a28eddc3993625177d
Nicks <1313nicov@gmail.com>
2022-04-04 12:47:15 -0500

    Descripción Pruebas Unitarias
    Se agregarpon el cocó de las pruebas faltantes y un breve comentario de que hace cada una de las pruebas

--- Pruebas_Cartas/UnitTest1.cs
+++ Pruebas_Cartas/UnitTest1.cs
@@ -11,52 +11,82 @@ namespace Pruebas_Cartas
         [TestMethod]
         public void TestDeck()
         {
-
+            //El Deck cumple con los límites de cartas 
+        }
+        [TestMethod]
+        public void TestDeckCP()
+        {
+            //El Deck no sobre pasa su CP al ser llenado
         }
         [TestMethod]
         public void TestCardAttributes() 
         { 
-
+            //Todas las cartas poeseen los atributos base
         }
         [TestMethod]
         public void TestChAttributes()
         {
-
+            //El Character posee todos los atributos
+        }
+        [TestMethod]
+        public void TestChAttack()
+        {
+            //El Character ataca a un Ch enemigo según sus atributos
+        }
+        [TestMethod]
+        public void TestChIsDestroy()
+        {
+            //Cuanndo su RP llega a cero el Character es destruido
         }
         [TestMethod]
         public void TestEqAttributes()
         {
-
+            //Equip posee todos los atributos
         }
         [TestMethod]
         public void TestEquipTA()
         {
-
+            //Solo afecta al atributo indicado en TA
         }
         [TestMethod]
         public void TestEquipEP()
         {
-
+            //Solo Modifica la cantidad indicada en EP
         }
         [TestMethod]
         public void TestEquipAffinity()
         {
-
+            //Se equipa solo cuando posee la misma afinidad que el Character
         }
         [TestMethod]
         public void TestSupportAttributes()
         {
-
+            //Conntiene todos los atributos
         }
         [TestMethod]
-        public void TestETSupport()
+        public void TestSupportET()
         {
-
+            //Hace solo el efecto indicado en ET
         }
         [TestMethod]
-        public void TestEPSupport()
+        public void TestSupportEP()
         {
-
+            //Modifica la cantidad indicada en Ep
+        }
+        [TestMethod]
+        public void TestETDestroyOlny1Card()
+        {
+            //DestroyEquip solo puede destruir una carta a la vez
+        }
+        [TestMethod]
+        public void TestCardPermaDeath()
+        {
+            //Nada de resurección, la destrucción de una carta es permamente  
+        }
+        [TestMethod]
+        public void TestGameOver()
+        {
+            //Condición: que se quede sin personajes
         }
     }
 }
