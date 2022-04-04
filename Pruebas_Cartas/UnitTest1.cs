34fc56abe01714a6a82642991d13270b2aac4b3e
Nicks <1313nicov@gmail.com>
2022-04-04 12:03:54 -0500

    Cocó de las prubas Unitarias
    Creé el cocó de las pruebas unitarias, con los nombres de cada una de las pruebas que consideramos necesarias

--- Pruebas_Cartas/UnitTest1.cs
+++ Pruebas_Cartas/UnitTest1.cs
@@ -2,11 +2,59 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Pruebas_Cartas
 {
-    [TestClass]
+    [TestClass] 
     public class UnitTest1
     {
+        //Valores de prueba:
+
+        //Pruebas unitarias para cada "combo" de valores
         [TestMethod]
-        public void TestMethod1()
+        public void TestDeck()
+        {
+
+        }
+        [TestMethod]
+        public void TestCardAttributes() 
+        { 
+
+        }
+        [TestMethod]
+        public void TestChAttributes()
+        {
+
+        }
+        [TestMethod]
+        public void TestEqAttributes()
+        {
+
+        }
+        [TestMethod]
+        public void TestEquipTA()
+        {
+
+        }
+        [TestMethod]
+        public void TestEquipEP()
+        {
+
+        }
+        [TestMethod]
+        public void TestEquipAffinity()
+        {
+
+        }
+        [TestMethod]
+        public void TestSupportAttributes()
+        {
+
+        }
+        [TestMethod]
+        public void TestETSupport()
+        {
+
+        }
+        [TestMethod]
+        public void TestEPSupport()
         {
 
         }
